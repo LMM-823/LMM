a30611159edb2568d675b9237bfd7ae2f3a6afbc
LMM <liewyuhao9@gmail.com>
2026-03-22 15:38:17 +0800

    更新 Core.lua

--- Core.lua
+++ Core.lua
@@ -73,7 +73,7 @@ local DiscordBtn = Instance.new("TextButton", ScrollingFrame)
 DiscordBtn.Size = UDim2.new(0.9, 0, 0, 50); DiscordBtn.BackgroundColor3 = Color3.fromRGB(60, 80, 200); DiscordBtn.Text = "JOIN DISCORD"; DiscordBtn.TextColor3 = Color3.new(1, 1, 1); Instance.new("UICorner", DiscordBtn)
 DiscordBtn.MouseButton1Click:Connect(function() setclipboard("https://discord.gg/cjpezEZub") end)
 
--- [[ 第三栏：基本功能刘某某 (名字已固定) ]]
+-- [[ 第三栏：刘某某基本功能 (仅在此处执行改名) ]]
 local FuncBtn = Instance.new("TextButton", ScrollingFrame)
 FuncBtn.Size = UDim2.new(0.9, 0, 0, 50); FuncBtn.BackgroundColor3 = Color3.fromRGB(35, 35, 35); FuncBtn.Text = "🌚 基本功能刘某某 🌚"; FuncBtn.TextColor3 = Color3.new(1, 1, 1); Instance.new("UICorner", FuncBtn)
 
