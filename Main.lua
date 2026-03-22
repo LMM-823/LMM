c1e303e3864dd88677b1d99dc17952cda85b41e2
LMM <liewyuhao9@gmail.com>
2026-03-22 16:26:47 +0800

    更新 Main.lua

--- Main.lua
+++ Main.lua
@@ -2,7 +2,7 @@
 -- 作用：远程加载主体 UI
 
 local function LoadCore()
-    -- 提示：请将下方的链接替换为你存放 Core.lua 的真实原始链接 (Raw Link)
+    -- 请将下方的链接替换为你存放 Core.lua 的真实原始链接 (Raw Link)
     local core_url = "你的_CORE_LUA_链接" 
     
     local success, result = pcall(function()
