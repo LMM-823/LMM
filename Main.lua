35fd9c97b174fd922b3b12f3af712f1bf0ab2ce0
LMM <liewyuhao9@gmail.com>
2026-03-22 16:59:44 +0800

    更新 Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,6 @@
 -- [[ 刘某某脚本库 | Main.lua ]]
+-- 作用：远程加载主体 UI
+
 local function LoadCore()
     -- 已自动接好你的 GitHub Raw 链接
     local core_url = "https://raw.githubusercontent.com/LMM-823/LMM/main/Core.lua" 
@@ -8,7 +10,7 @@ local function LoadCore()
     end)
     
     if not success then
-        warn("加载失败: " .. tostring(result))
+        warn("刘某某脚本库加载失败: " .. tostring(result))
     end
 end
 
