a1ee0f8101a762da578ddead2042e658082f249b
LMM <liewyuhao9@gmail.com>
2026-03-22 16:41:42 +0800

    更新 Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,7 @@
 -- [[ 刘某某脚本库 | Main.lua ]]
 local function LoadCore()
-    -- ⚠️ 提示：请将下方的链接替换为你 GitHub 上 Core.lua 的 Raw 原始链接
-    local core_url = "你的_CORE_LUA_RAW链接" 
+    -- 已为你填入 Core.lua 的 Raw 原始链接
+    local core_url = "https://raw.githubusercontent.com/LMM-823/LMM/main/Core.lua" 
     
     local success, result = pcall(function()
         return loadstring(game:HttpGet(core_url))()
